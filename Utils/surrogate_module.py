684c9f5a27037e292a5b14c4994dbfdc268d25d4
MatkinGurza <gurza.mateus@usp.br>
2026-07-28 01:58:43 -0300

    monografia update about surrogates and optimization approaches

---
+++ Utils/surrogate_module.py
@@ -0,0 +1,7 @@
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import List, Dict, Any, Literal, Optional, Tuple, Union
+from dataclasses import dataclass, field
+
